fc1beef923770dbef6a9d4302e6549e3667dc96b
Erik Benson <efbenson@gmail.com>
2015-01-19 00:12:10 -0500

    Changes

---
+++ MapSharpGen/Generate.cs
@@ -0,0 +1,16 @@
+﻿namespace MapSharpGen
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// The generate container.
+    /// </summary>
+    public class MapGenerator
+    {
+
+    }
+}
